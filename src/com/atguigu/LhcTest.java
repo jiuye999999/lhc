e2c3debf9647e03a78f3c51d78e7d2e13e9605a8
jiuye9999 <ninexz@163.com>
2017-07-05 00:42:40 +0800

    fdsa

--- src/com/atguigu/LhcTest.java
+++ src/com/atguigu/LhcTest.java
@@ -4,5 +4,10 @@ public class LhcTest {
 
 	public static void main(String[] args) {
 		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
 	}
 }
